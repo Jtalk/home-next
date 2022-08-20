83407b8ac5ad87cadfe30e3314df6fdbe53188d4
Roman Nazarenko <me@jtalk.me>
2022-08-20 20:43:53 +0100

    Barebone project with Next.js and Mui

---
+++ ui/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from "@mui/material";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      <Head>what</Head>
+      <main>Hello</main>
+    </Container>
+  );
+};
+
+export default Home;
